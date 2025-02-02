d769684e4eb3f4c5c4c9e83af8c1615a7178feac
Venuja Prasanjith <144810283+VenujaVP@users.noreply.github.com>
2025-02-02 20:25:55 +0530

    Splash Screen update 04

--- App.js
+++ App.js
@@ -7,10 +7,12 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-  <>
-
-  
-  </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} /> {/* Your next screen */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
